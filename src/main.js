b3af63b214adf7d28c9f3ffda0cbf716d9a73c4d
Yuan <xxx871022@gmail.com>
2025-11-29 22:47:06 +0800

    新增vercel speed insights

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 createApp(App).use(router).mount('#app')
